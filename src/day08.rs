28cf76f57928eb7f332b4ddbf01500a77e1089b2
Andreas Ellwanger <andreas.ellwanger@campus.lmu.de>
2021-12-08 17:51:16 +0100

    Add day08 task1

--- src/day08.rs
+++ src/day08.rs
@@ -1,5 +1,33 @@
+use std::collections::HashSet;
+
+pub fn unique_numbers() -> HashSet<i32> {
+    let mut unique_numbers = HashSet::new();
+    unique_numbers.insert(2); // 1
+    unique_numbers.insert(4); // 4
+    unique_numbers.insert(3); // 7
+    unique_numbers.insert(7); // 8
+    unique_numbers
+}
+
 pub fn part1(input: &String) -> i64 {
-    0
+    let mut count = 0;
+    let unique_numbers = unique_numbers();
+    for line in input.lines() {
+        let output_values: Vec<&str> =  line
+            .trim()
+            .split('|')
+            .last()
+            .unwrap()
+            .trim()
+            .split(' ')
+            .collect();
+        for val in output_values {
+            if unique_numbers.contains(&(val.len() as i32)) {
+                count += 1;
+            }
+        }
+    }
+    count
 }
 
 pub fn part2(input: &String) -> i64 {
@@ -11,7 +39,7 @@ mod tests {
     use crate::read_input;
 
     const DAY: u8 = 8;
-    const VALID_ANSWER_PART_1: i64 = 0;
+    const VALID_ANSWER_PART_1: i64 = 278;
     const VALID_ANSWER_PART_2: i64 = 0;
 
     #[test]
